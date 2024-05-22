1d817e2e10d3a2b2ba0052b30caceeb319e2b226
Dan Paulat <dpaulat@gmail.com>
2024-05-21 22:39:29 -0500

    Redirect mbgl platform log to proper Qt message type

--- platform/qt/src/mbgl/logging_qt.cpp
+++ platform/qt/src/mbgl/logging_qt.cpp
@@ -1,12 +1,29 @@
 #include <mbgl/util/logging.hpp>
 #include <mbgl/util/enum.hpp>
 
+#include <unordered_map>
+
 #include <QDebug>
 
 namespace mbgl {
 
 void Log::platformRecord(EventSeverity severity, const std::string &msg) {
-    qWarning() << "[" << Enum<EventSeverity>::toString(severity) << "] " << QString::fromStdString(msg);
+    static const std::unordered_map<EventSeverity, QtMsgType> levelMap_{
+        {EventSeverity::Debug, QtMsgType::QtDebugMsg},
+        {EventSeverity::Info, QtMsgType::QtInfoMsg},
+        {EventSeverity::Warning, QtMsgType::QtWarningMsg},
+        {EventSeverity::Error, QtMsgType::QtCriticalMsg}};
+
+    static const QMessageLogContext context(nullptr, 0, nullptr, "mbgl");
+
+    QtMsgType msgType = QtMsgType::QtInfoMsg;
+
+    auto it = levelMap_.find(severity);
+    if (it != levelMap_.cend()) {
+        msgType = it->second;
+    }
+
+    qt_message_output(msgType, context, QString::fromStdString(msg));
 }
 
 } // namespace mbgl
